0d2fc1743c6a67625a2d84f3baa13f98c1529f8a
Colin Seaman <colin.seaman@diaku.com>
2016-11-17 07:22:04 -0800

    Initial Commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = new React.createClass({
+
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Weather App</li>
+
+            <li><Link to="/timer" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</Link></li>
+            <li><Link to="/countdown" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link></li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">by Colin Seaman</li>
+          </ul>
+
+        </div>
+      </div>
+    )
+  }
+
+});
+
+module.exports = Nav;
